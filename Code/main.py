5830b1d53bbb388fe1674ee2c3ee197723bb6c65
avinav291 <avinav291@gmail.com>
2017-03-20 18:34:05 +0530

    Overfitting.py Added 98.91 Achieved

--- Code/main.py
+++ Code/main.py
@@ -15,10 +15,20 @@
 # Accuracy 95.1%
 import dataset_loader
 import network2
+import overfitting
 
-training_data = dataset_loader.loadDataTrain('../Dataset/poker-hand-training-true.csv')
-test_data = dataset_loader.loadDataTest('../Dataset/poker-hand-testing.csv')
+# training_data = dataset_loader.loadDataTrain('../Dataset/poker-hand-training-true.csv')
+# test_data = dataset_loader.loadDataTest('../Dataset/poker-hand-testing.csv')
 
-net = network2.Network([85, 20, 20, 10], cost=network2.CrossEntropyCost)
-net.large_weight_initializer()
-net.SGD(training_data, 100, 10, 0.5,lmbda=0.5, evaluation_data=test_data, monitor_evaluation_accuracy=True)
+# net = network2.Network([85, 20, 20, 10], cost=network2.CrossEntropyCost)
+# net.large_weight_initializer()
+# net.SGD(training_data, 100, 10, 0.5,lmbda=0.5, evaluation_data=test_data, monitor_evaluation_accuracy=True, monitor_evaluation_cost=True
+#         , monitor_training_accuracy=True, monitor_training_cost=True)
+
+overfitting.main(filename='results.txt', num_epochs=100,
+         training_cost_xmin=200,
+         test_accuracy_xmin=200,
+         test_cost_xmin=0,
+         training_accuracy_xmin=0,
+         training_set_size=25000,
+         lmbda=0.5)
